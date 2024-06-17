8de7aa2f943681c10612cadbd5ba54ab6b9005e5
ronakbhutiya <ronakbhutiya123@gmail.com>
2024-06-17 09:29:59 +0530

    change btn color

--- js.js
+++ js.js
@@ -6,11 +6,14 @@ btn.addEventListener("click", () => {
   if (bcColor === true) {
     body.style.backgroundImage =
       "linear-gradient(40deg, #24404c 50%, #2fbeb4 50%)";
+
     // console.log("rinak");
     bcColor = false;
   } else {
     body.style.backgroundImage =
       "linear-gradient(40deg, #2fbeb4 50%, #24404c 50%)";
+    btn.style.backgroundColor = "#24404c";
+
     // console.log("ronak");
     bcColor = true;
   }
